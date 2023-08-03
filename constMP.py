42b7e8b90466b274c6d380db3d621618ed30ee06
lguilhermebarbosa <133176331+lguilhermebarbosa@users.noreply.github.com>
2023-08-02 21:15:20 -0300

    Update constMP.py

--- constMP.py
+++ constMP.py
@@ -1,7 +1,7 @@
 #To Do: Replace all this with a naming service
 PEERS = ['172.31.81.134','172.31.86.140','172.31.95.254','172.31.92.7','172.31.90.232','172.31.88.90']
-PORT = 5001
+PORT = 5002
 N = 6
 N_MSGS = 100
 SERVER_ADDR = '172.31.90.114'
-SERVER_PORT = 5000
+SERVER_PORT = 5001
